c4c5e4bf491ed78e4bbc7b8288aa35655bf770d7
RubiAlv <124765356+RubiAlv@users.noreply.github.com>
2023-12-01 04:01:58 -0600

    first commit

---
+++ inc/container.php
@@ -0,0 +1,35 @@
+</head>
+<body class="">
+<!--<div role="navigation" class="navbar navbar-default navbar-static-top">-->
+<div role="navigation" class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="/web/index.html" class="navbar-brand">TecnoCel</a>
+        </div>
+        <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="/web/index.html">Inicio</a></li>
+           
+          </ul>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/web/productos.php">Productos</a></li>
+           
+          </ul>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/web/faqs.html">Preguntas frecuentes</a></li>
+           
+          </ul>
+         
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	
+	<div class="container" style="min-height:500px;">
+	<div class=''>
+	</div>
