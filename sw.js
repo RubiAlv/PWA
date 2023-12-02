e81e847ad6529d404488e85e64eaec343703a0f6
RubiAlv <124765356+RubiAlv@users.noreply.github.com>
2023-12-02 13:13:24 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ self.addEventListener('install', (event) => {
       caches.open('mi-pwa-cache').then((cache) => {
         return cache.addAll([
           '/index.html',
-          '/views/mision.html',
-          '/css/mision.css',
-          '/imagenes/6.jpg',
-          'imagenes/7.jpg'
+          '/views',
+          '/css',
+          '/imagenes',
+          '/js'
           // Agrega aquí todos los archivos que quieres cachear
         ]);
       })
