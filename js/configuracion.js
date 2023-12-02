936da8148ecbf77cb8f4a2848912f0cdc90d244c
RubiAlv <124765356+RubiAlv@users.noreply.github.com>
2023-12-02 16:22:53 -0600

    conexion db

---
+++ js/configuracion.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAataMJlVggnMDowG0Mx9QUQ-HAhQveRro",
+    authDomain: "pwacafe.firebaseapp.com",
+    projectId: "pwacafe",
+    storageBucket: "pwacafe.appspot.com",
+    messagingSenderId: "902868940998",
+    appId: "1:902868940998:web:89f310e34e25e04d41ffc7"
+});
+var db = firebase.firestore();
